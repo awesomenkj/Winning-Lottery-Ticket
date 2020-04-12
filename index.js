05495db7f5de5a7b9efa43e64343b59f95149ad1
Peter Rabbit <de.nimitz@yandex.com>
2020-04-12 02:05:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+function winningLotteryTicket(tickets) {
+    const masks = [];
+    let i = 0, count = 0;
+    let mask = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    
+    while (tickets.length > 0) {    
+        if (tickets[0].includes(`${i}`)) mask[i] = 1; // check if ticket includes i
+        if (i == 9) {
+            i = 0;
+            tickets.shift();
+            masks.push(parseInt(mask.join(''), 2));
+            // masks.push(mask.join(''));
+            mask = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            continue;
+        }
+        i ++;
+    }
 
+    while (masks.length > 1) {
+        
+        if (parseInt(masks[0], 2) === 1023) {
+            count = count + masks.length - 1;
+            masks.shift();
+            i = 1;
+            continue;
+        }
+        
+        if ((masks[0] | masks[i]) === 1023) count++;
+ 
+        if (i == (masks.length - 1)) {
+            i = 1;
+            masks.shift();
+            continue;
+        }
+        i++;
+        
+    }
+    
+    return count;
+}
